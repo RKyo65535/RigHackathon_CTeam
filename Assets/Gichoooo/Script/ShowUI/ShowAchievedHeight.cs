6e8475df5d53b9271cc190408df9b84b1d921b13
Taki <prgrm2960.17chan@gmail.com>
2020-09-10 23:26:01 +0900

    到達度システムを開発

---
+++ Assets/Gichoooo/Script/ShowUI/ShowAchievedHeight.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ShowAchievedHeight : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    [SerializeField] int StartHeight;
+    [SerializeField] int GoalHeight;
+
+
+
+    Text text;
+
+    private void Awake()
+    {
+        text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int achievedHeight;
+        achievedHeight = (int)((player.transform.position.y - StartHeight)*100/(GoalHeight - StartHeight));
+
+        text.text = "到達度 : "+achievedHeight+"%";
+    }
+}
