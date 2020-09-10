2cb50bc4434f24aba2c99398b5a49efcb09731fd
Taki <prgrm2960.17chan@gmail.com>
2020-09-10 21:43:28 +0900

    基本的な機能の追加

---
+++ Assets/Gichoooo/Script/PlayerMove/HitFallObject.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitFallObject : MonoBehaviour
+{
+    private PlayerJetPack playerJet;
+    private Rigidbody2D playerRigidBody;
+
+    [SerializeField] float RockHitPunishmentPower;
+    [SerializeField] float MagmaHitPunishmentPower;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        playerJet = GetComponent<PlayerJetPack>();
+        playerRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Obstacle")
+        {
+            playerRigidBody.velocity = new Vector2(0, -RockHitPunishmentPower);
+            playerJet.canControl = false;
+        }
+        else if (collision.tag == "Magma")
+        {
+            float arg =Random.value*360 * Mathf.Deg2Rad;
+
+            playerRigidBody.velocity = new Vector2(MagmaHitPunishmentPower * Mathf.Cos(arg), MagmaHitPunishmentPower * Mathf.Sin(arg));
+            playerJet.canControl = false;
+        }
+
+
+
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if(collision.transform.tag == "Floor")
+        {
+            playerRigidBody.velocity = new Vector2(0, playerRigidBody.velocity.y);//着地したら横方向の加速は０に
+            playerJet.canControl = true;
+        }
+
+
+
+    }
+
+
+
+}
--- Assets/Gichoooo/Script/PlayerMove/PlayerJetPack.cs
+++ Assets/Gichoooo/Script/PlayerMove/PlayerJetPack.cs
@@ -1,11 +1,22 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerJetPack : MonoBehaviour
 {
+    public bool canControl;
+
     private Rigidbody2D playerRigidBody;
     private Transform playerTransForm;
+
+    [SerializeField] float HorizonalSpeed;
+    [SerializeField] float VerticalSpeed;
+    [SerializeField] float VerticalMaxSpeed;
+    [SerializeField] float VerticalMinimumGuaranteeSpeed;//上方向加速時の最低保証スピード
+
+    [NonSerialized] public int electricPower;
+    [SerializeField] int maxElectricPower;
 
     class PushButton
     {
@@ -16,8 +27,10 @@ public class PlayerJetPack : MonoBehaviour
         public bool pushLeft;
         public bool pushRight;
         public bool pushUp;
-
+        public bool pushDown;
+        public bool onChain;
     }
+    PushButton pushButton;
 
 
     // Start is called before the first frame update
@@ -25,20 +38,170 @@ public class PlayerJetPack : MonoBehaviour
     {
         playerRigidBody = GetComponent<Rigidbody2D>();
         playerTransForm = GetComponent<Transform>();
+        pushButton = new PushButton();
+        canControl = true;
+
+        electricPower = maxElectricPower;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        //
+        if(playerRigidBody.velocity == Vector2.zero)
+        {
+            electricPower += 3;
+            CheckMaxEletricPower();
+        }
 
-        
+        if (!canControl)//コントロール不可能状態なら、ボタン入力を受け付けない
+        {
+            return;
+        }
+
+        //ボタン読み込み
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            pushButton.pushSpace = true;
+        }
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            pushButton.pushZ = true;
+        }
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            pushButton.pushX = true;
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            pushButton.pushC = true;
+        }
+
+
+        //十字入力
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            pushButton.pushLeft = true;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            pushButton.pushRight = true;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            pushButton.pushUp = true;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            pushButton.pushDown = true;
+        }
     }
 
     private void FixedUpdate()
     {
-        
+        if (pushButton.onChain)//チェーンを持っている場合
+        {
+            pushButton.onChain = false;
+            if (pushButton.pushUp)
+            {
+                pushButton.pushUp = false;
+                playerTransForm.position += new Vector3(0, HorizonalSpeed, 0);
+            }
+
+            if (pushButton.pushDown)
+            {
+                pushButton.pushDown = false;
+                playerTransForm.position += new Vector3(0, -HorizonalSpeed/2, 0);
+            }
+
+        }
+
+
+        if (pushButton.pushRight)
+        {
+            pushButton.pushRight = false;
+            playerTransForm.position += new Vector3(HorizonalSpeed, 0,0) ;
+
+        }
+
+        if (pushButton.pushLeft)
+        {
+            pushButton.pushLeft = false;
+            playerTransForm.position += new Vector3(-HorizonalSpeed, 0, 0);
+
+        }
+
+        if (pushButton.pushUp)
+        {
+            pushButton.pushUp = false;
+            if (electricPower > 0)
+            {
+                electricPower--;
+
+                if (playerRigidBody.velocity.y < VerticalMinimumGuaranteeSpeed)
+                {
+                    playerRigidBody.velocity = new Vector2(playerRigidBody.velocity.x, VerticalMinimumGuaranteeSpeed);
+                }
+
+                playerRigidBody.velocity += new Vector2(0, VerticalSpeed);
+
+                if (playerRigidBody.velocity.y > VerticalMaxSpeed)
+                {
+                    playerRigidBody.velocity = new Vector2(playerRigidBody.velocity.x, VerticalMaxSpeed);
+                }
+            }
+        }
+        else//上を押してないなら、急に減速
+        {
+            if (playerRigidBody.velocity.y > 6)
+            {
+                playerRigidBody.velocity -= new Vector2(0, 1f);
+            }
+        }
+
+
+        if (pushButton.pushDown)
+        {
+            pushButton.pushDown = false;
+        }
+
+
+        if (pushButton.pushZ)
+        {
+            pushButton.pushZ = false;
+        }
+
+        if (pushButton.pushX)
+        {
+            pushButton.pushX = false;
+        }
+
+        if (pushButton.pushC)
+        {
+            pushButton.pushC = false;
+        }
+
+
+    }
+
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Chain")
+        {
+            playerRigidBody.velocity = new Vector2(0, 0);
+            canControl = true;
+            pushButton.onChain = true;
+        }
+    }
+
+
+    public void CheckMaxEletricPower()
+    {
+        if (electricPower > maxElectricPower)
+        {
+            electricPower = maxElectricPower;
+        }
     }
 
 }
