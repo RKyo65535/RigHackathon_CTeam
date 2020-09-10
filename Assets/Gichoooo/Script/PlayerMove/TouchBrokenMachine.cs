6d0f0ff9d9477bd0e21fb50fab04bf838a15dea2
Taki <prgrm2960.17chan@gmail.com>
2020-09-11 00:17:52 +0900

    メッセージシステムの開発

---
+++ Assets/Gichoooo/Script/PlayerMove/TouchBrokenMachine.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class TouchBrokenMachine : MonoBehaviour
+{
+    [SerializeField] GameObject canvas;
+    [SerializeField] Text text;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "BrokenMachine")
+        {
+            canvas.SetActive(true);
+            text.text = collision.GetComponent<BrekenMachineMessageList>().GetMessage();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "BrokenMachine")
+        {
+            canvas.SetActive(false);
+        }
+    }
+
+}
