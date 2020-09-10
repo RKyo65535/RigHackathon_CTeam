dfdf4b6837c126208a6942022a8fffc51dba63fb
Taki <prgrm2960.17chan@gmail.com>
2020-09-10 18:19:50 +0900

    add Player Script

---
+++ Assets/Gichoooo/Script/PlayerMove/PlayerJetPack.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJetPack : MonoBehaviour
+{
+    private Rigidbody2D playerRigidBody;
+    private Transform playerTransForm;
+
+    class PushButton
+    {
+        public bool pushZ;
+        public bool pushX;
+        public bool pushC;
+        public bool pushSpace;
+        public bool pushLeft;
+        public bool pushRight;
+        public bool pushUp;
+
+    }
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        playerRigidBody = GetComponent<Rigidbody2D>();
+        playerTransForm = GetComponent<Transform>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //
+
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+}
