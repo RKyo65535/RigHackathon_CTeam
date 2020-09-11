759cfada4672458ed5eef464f8231c88a8dd1c09
Taki <prgrm2960.17chan@gmail.com>
2020-09-11 13:25:45 +0900

    修正

---
+++ Assets/Gichoooo/Script/ShowUI/ShowAchievePer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ShowAchievePer : MonoBehaviour
+{
+    Text text;
+    Transform TF;
+    Vector3 firstPos;
+    [SerializeField] GameObject player;
+    [SerializeField] int StartHeight;
+    [SerializeField] int GoalHeight;
+
+
+
+    private void Awake()
+    {
+        text = GetComponent<Text>();
+        TF = GetComponent<Transform>();
+        firstPos = TF.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int achievedHeight;
+        achievedHeight = (int)((player.transform.position.y - StartHeight) * 100 / (GoalHeight - StartHeight));
+        text.text = "" + achievedHeight;
+    }
+}
