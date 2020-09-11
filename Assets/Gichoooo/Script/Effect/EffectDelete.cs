91f76ce04b15255488bc372c292cde2fd92e872f
Taki <prgrm2960.17chan@gmail.com>
2020-09-11 18:18:56 +0900

    背景を強化、えっふぇくとを追加

---
+++ Assets/Gichoooo/Script/Effect/EffectDelete.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectDelete : MonoBehaviour
+{
+    float passedTime;
+    [SerializeField] float deathTime;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        passedTime = Time.deltaTime;
+        passedTime = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        passedTime += Time.deltaTime;
+        if(passedTime > deathTime)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Gichoooo/Script/Title/GotoOpening.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GotoOpening : MonoBehaviour
+{
+    Button button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(()=>MovetoOpening());
+    }
+
+    void MovetoOpening()
+    {
+        SceneManager.LoadScene("Opening");
+    }
+
+}
